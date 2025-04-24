13a2ca41fd4bbab8589044dc302ef7d8c6a4fbe6
FraDev85 <comunalefrancesco@gmail.com>
2025-04-24 14:43:20 +0200

    aggiunta files

---
+++ public/js/loader.js
@@ -0,0 +1,14 @@
+export function loadImage(url) {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.addEventListener("load", () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+
+export async function loadLevel(name) {
+  const r = await fetch(`/levels/${name}.json`);
+  return r.json();
+}
